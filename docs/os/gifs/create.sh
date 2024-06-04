a886fea6084267925d8b3c1e63b43e50d38d858d
Tiago <ercolealfageme@hotmail.com>
2024-06-04 14:45:28 +0200

    Restored gifs yml

---
+++ docs/os/gifs/create.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Create all the terminalizer stuff
+#
+np=1
+if [[ $# -gt 0 ]]; then
+  np=$1
+  shift
+fi
+
+run=commands.run
+rm -f $run
+mkdir -p tmp
+echo "Running creation of $run script"
+python3 merge.py $run
+
+echo "Will run:"
+cat $run
+echo "in parallel"
+
+if [[ $np -eq 1 ]]; then
+  while read line ; do
+    echo "Running: ${line}"
+    ${line}
+  done < $run
+else
+  parallel -j$np < $run
+fi
